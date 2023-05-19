23ff419efeb373e4101552bfffefdbbc93119051
Zarpas <imartin1974@gmail.com>
2023-05-19 16:23:51 +0200

    update

--- core/auth/__init__.py
+++ core/auth/__init__.py
@@ -10,10 +10,29 @@ from flask_jwt_extended import (
     unset_jwt_cookies,
 )
 
-from .. import app
+from .. import db
 from ..models import User, user_schema
 
 auth = Blueprint("auth", __name__)
+
+
+@auth.route("/register", methods=["POST"])
+def register():
+    id = request.json.get("id")
+    name = request.json.get("name")
+    surnames = request.json.get("surnames")
+    email = request.json.get("email")
+    password = request.json.get("password")
+
+    user = User(id=id, name=name, surnames=surnames, email=email, password=password)
+
+    if user is None:
+        return jsonify({"msg": "Something went wrong with user register"}), 400
+    
+    db.session.add(user)
+    db.session.commit()
+    return user_schema.jsonify(user), 201
+
 
 
 @auth.route("/auth", methods=["POST"])
@@ -61,5 +80,58 @@ def logout():
 @auth.route("/example", methods=["GET"])
 @jwt_required()
 def protected():
-    username = get_jwt_identity()
-    return jsonify({"hello": "from {}".format(username)}), 200
+    id = get_jwt_identity()
+    user = User.query.get(id)
+    return jsonify({"hello": "from {}".format(user.name + ' ' + user.surnames)}), 200
+
+
+@auth.route("/new_password", methods=["POST"])
+@jwt_required()
+def new_password():
+    id = get_jwt_identity()
+    user = User.query.get(id)
+    old_password = request.json.get("old_password")
+    new_password = request.json.get("new_password")
+    if user.verify_password(old_password):
+        user.password_hash = user.hash_password(new_password)
+        db.session.add(user)
+        db.session.commit()
+        return user_schema.jsonify(user), 200
+    
+    return jsonify({"msg": "password incorrect"}), 400
+
+
+@auth.route("/user_admin", methods=["POST"])
+@jwt_required()
+def user_admin():
+    id = get_jwt_identity()
+    admin = User.query.get(id)
+
+    if admin.can_admin is False:
+        return jsonify({"msg": "not necessary rights for this"}), 401
+    
+    user_id = request.json.get("id")
+    user = User.query.get(user_id)
+    can_admin = request.json.get("can_admin", None)
+    if can_admin is not None:
+        user.can_admin = can_admin
+    can_fileupload = request.json.get("can_fileupload", None)
+    if can_fileupload is not None:
+        user.can_fileupload = can_fileupload
+    can_listoperate = request.json.get("can_listoperate", None)
+    if can_listoperate is not None:
+        user.can_listoperate = can_listoperate
+    can_writenote = request.json.get("can_writenote", None)
+    if can_writenote is not None:
+        user.can_writenote = can_writenote
+    can_takepicture = request.json.get("can_takepicture", None)
+    if can_takepicture is not None:
+        user.can_takepicture = can_takepicture
+    active = request.json.get("active", None)
+    if active is not None:
+        user.active = active
+
+    db.session.add(user)
+    db.session.commit()
+    return user_schema.jsonify(user), 200
+
